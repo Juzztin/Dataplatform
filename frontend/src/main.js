92da9a3876dae3345392a15e3d0a0826ab16f09e
Juzztin <73253113+Juzztin@users.noreply.github.com>
2022-11-14 13:44:34 -0600

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 import './index.css'
+import VueChartkick from 'vue-chartkick'
+import 'chartkick/chart.js'
 
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+// Using https://chartkick.com/vue for Chart formatting
+app.use(VueChartkick)
