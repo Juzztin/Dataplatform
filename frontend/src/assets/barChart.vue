97618b54ff8c449d5562584bc6baef321e629a1c
Juzztin <73253113+Juzztin@users.noreply.github.com>
2022-11-14 13:47:57 -0600

    Create barChart.vue

---
+++ frontend/src/assets/barChart.vue
@@ -0,0 +1,40 @@
+  </template>
+  
+  <script>
+  import { Chart, registerables } from 'chart.js'
+  
+  Chart.register(...registerables);
+  export default {
+    props: {
+      label: {
+        type: Array,
+      },
+      chartData: {
+        type: Array,
+      },
+    },
+    async mounted() {
+      console.log(this.chartData);
+      await new Chart(this.$refs.myChart, {
+        type: "bar",
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              label: "Attendee Total",
+              backgroundColor: "rgba(144,238,144 , 0.9 )",
+              data: this.chartData,
+            },
+          ],
+        },
+        options: {
+          scales: {
+              y: {
+                  min: 0
+              }
+          }
+        }
+      });
+    },
+  };
+  </script>
